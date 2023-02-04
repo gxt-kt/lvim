60e1c32a33476cffda7adac28182d4b5835b9e22
gxt_kt <gxt_kt.com>
2023-02-05 06:41:00 +0800

    add rainbow bracket extend_mode enable

--- config.lua
+++ config.lua
@@ -326,6 +326,7 @@ lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enable = true
 
 lvim.builtin.treesitter.rainbow.enable = true
+lvim.builtin.treesitter.rainbow.extended_mode = true
 
 -- generic LSP settings
 
